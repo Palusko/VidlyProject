9a13b1c703d937ceca47731e78852c0e86e681e1
Pavol Almasi <pavol_the_best@hotmail.com>
2017-12-30 23:39:51 -0500

    Initial Commit

---
+++ VidlyProject/VidlyProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyProject.Startup))]
+namespace VidlyProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
