b915235524bf755fad5115e26360e0b94847456d
Pavol Almasi <pavol_the_best@hotmail.com>
2018-01-18 19:52:59 -0500

    Added Ajax and Datatable

--- VidlyProject/VidlyProject/App_Start/BundleConfig.cs
+++ VidlyProject/VidlyProject/App_Start/BundleConfig.cs
@@ -21,10 +21,14 @@ namespace VidlyProject
 
       bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                "~/Scripts/bootbox.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/datatables/jquery.datatables.js",
+                "~/Scripts/datatables/datatables.bootstrap.js"));
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap-cyborg.css",
+                "~/Content/datatables/css/datatables.bootstrap.css",
                 "~/Content/site.css"));
     }
   }
